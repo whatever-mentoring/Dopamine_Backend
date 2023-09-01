e05cc550f1e2151ba803f1e0615d87cf00535f04
KarmaPol <kkhkr98@gmail.com>
2023-09-01 17:15:24 +0900

    build-#3 : add connection to mysql db
    add yml and gradle file to connect with naver cloud db

---
+++ src/main/java/dopamine/backend/domain/test/Test.java
@@ -0,0 +1,13 @@
+package dopamine.backend.domain.test;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
