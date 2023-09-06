42f87096361ca2720534d2e9168e4557c64229f5
gabang2 <offbeat1020@naver.com>
2023-09-07 01:55:37 +0900

    feat-#1 : jwt구현

---
+++ src/main/java/dopamine/backend/jwt/config/AuthenticationConfig.java
@@ -0,0 +1,42 @@
+package dopamine.backend.jwt.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dopamine.backend.jwt.filter.JwtExceptionFilter;
+import dopamine.backend.jwt.filter.JwtFilter;
+import dopamine.backend.jwt.service.OauthService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class AuthenticationConfig {
+
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt 사용하는 경우 사용
+                .and()
+                .authorizeRequests(authorize -> authorize.antMatchers("/api/oauth/kakao/**").permitAll())
+                //.authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .addFilterBefore(new JwtFilter(secretKey), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(objectMapper), JwtFilter.class)
+                .build()
+                ;
+    }
+}
---
+++ src/main/java/dopamine/backend/jwt/service/JwtService.java
@@ -0,0 +1,34 @@
+package dopamine.backend.jwt.service;
+
+
+import dopamine.backend.jwt.provider.JwtProvider;
+import dopamine.backend.member.entity.Member;
+import dopamine.backend.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final MemberService memberService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String getAccessToken(Member member){
+
+        String refreshToken = JwtProvider.createRefreshToken(member.getId(), secretKey);
+        member.setRefreshToken(refreshToken);
+        memberService.patchMember(member);
+
+        return "Bearer " + JwtProvider.createAccessToken(member.getId(), secretKey);
+    }
+
+    public Member getMemberFromAccessToken(String accessToken) {
+        accessToken = accessToken.split(" ")[2];
+        Long userId = JwtProvider.getUserId(accessToken, secretKey);
+        return memberService.verifiedMember(userId);
+    }
+}
