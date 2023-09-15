78e8fa0c7e2005a528ba56a4cd6435ec636cdca2
KarmaPol <kkhkr98@gmail.com>
2023-09-16 00:22:29 +0900

    feat : add feedlistbymember test code

--- src/test/java/dopamine/backend/feed/service/FeedServiceTest.java
+++ src/test/java/dopamine/backend/feed/service/FeedServiceTest.java
@@ -5,9 +5,13 @@ import dopamine.backend.feed.mapper.FeedMapper;
 import dopamine.backend.feed.repository.FeedRepository;
 import dopamine.backend.feed.response.FeedResponseDTO;
 import dopamine.backend.feedLike.service.FeedLikeService;
+import dopamine.backend.level.entity.Level;
 import dopamine.backend.level.repository.LevelRepository;
+import dopamine.backend.level.request.LevelRequestDto;
 import dopamine.backend.level.service.LevelService;
+import dopamine.backend.member.entity.Member;
 import dopamine.backend.member.repository.MemberRepository;
+import dopamine.backend.member.request.MemberRequestDto;
 import dopamine.backend.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -69,43 +73,91 @@ class FeedServiceTest {
         assertThat(feedResponseDTOS.size()).isEqualTo(9);
     }
 
-//    @Test
-//    @DisplayName("인증글 리스트 - 좋아요 순")
-//    void feedlistbylikecount(){
-//
-//        // given
-//        MemberRequestDto requestDto = MemberRequestDto.builder().nickname("test").build();
-//        LevelRequestDto levelRequestDto = LevelRequestDto.builder().name("testLev").exp(5).build();
-//        Level level = levelService.createLevel(levelRequestDto);
-//        Member member = memberService.createMember(requestDto);
-//
-//        levelRepository.save(level);
-//        memberRepository.save(member);
-//
-//        Feed feed1 = Feed.builder().content("1").build();
-//        Feed feed2 = Feed.builder().content("2").build();
-//        Feed feed3 = Feed.builder().content("3").build();
-//        Feed feed4 = Feed.builder().content("4").build();
-//        Feed feed5 = Feed.builder().content("5").build();
-//        Feed feed6 = Feed.builder().content("6").build();
-//        Feed feed7 = Feed.builder().content("7").build();
-//        Feed feed8 = Feed.builder().content("8").build();
-//        Feed feed9 = Feed.builder().content("9").build();
-//
-//        feedRepository.saveAll(List.of(feed1,feed2,feed3,feed4,feed5,feed6,feed7,feed8,feed9));
-//
-//        Member findMember = memberRepository.findMemberByNickname("test").orElseThrow(() -> new RuntimeException("찾을 수 없음"));
-//        Long memberId = findMember.getMemberId();
-//
-//        Feed feed = feedRepository.findFeedByContent("9").orElseThrow(() -> new RuntimeException("찾을 수 없음"));
-//        Long feedId = feed.getFeedId();
-//
-//        // when
-//        feedLikeService.feedLike(feedId, memberId);
-//        List<FeedResponseDTO> feedResponseDTOS = feedService.feedListOrderByLikeCount(1);
-//
-//        // then;
-//        assertThat(feedResponseDTOS.get(0)).isEqualTo(feed9);
-//        assertThat(feedResponseDTOS.size()).isEqualTo(9);
-//    }
+    @Test
+    @DisplayName("인증글 리스트 - 좋아요 순")
+    void feedlistbylikecount(){
+
+        // given
+        MemberRequestDto requestDto = MemberRequestDto.builder().nickname("test").build();
+        LevelRequestDto levelRequestDto = LevelRequestDto.builder().name("testLev").exp(5).build();
+        Level level = levelService.createLevel(levelRequestDto);
+        Member member = memberService.createMember(requestDto);
+
+        levelRepository.save(level);
+        memberRepository.save(member);
+
+        Feed feed1 = Feed.builder().content("1").build();
+        Feed feed2 = Feed.builder().content("2").build();
+        Feed feed3 = Feed.builder().content("3").build();
+        Feed feed4 = Feed.builder().content("4").build();
+        Feed feed5 = Feed.builder().content("5").build();
+        Feed feed6 = Feed.builder().content("6").build();
+        Feed feed7 = Feed.builder().content("7").build();
+        Feed feed8 = Feed.builder().content("8").build();
+        Feed feed9 = Feed.builder().content("9").build();
+
+        feedRepository.saveAll(List.of(feed1,feed2,feed3,feed4,feed5,feed6,feed7,feed8,feed9));
+
+        Member findMember = memberRepository.findMemberByNickname("test").orElseThrow(() -> new RuntimeException("찾을 수 없음"));
+        Long memberId = findMember.getMemberId();
+
+        Feed feed = feedRepository.findFeedByContent("9").orElseThrow(() -> new RuntimeException("찾을 수 없음"));
+        Long feedId = feed.getFeedId();
+
+        // when
+        feedLikeService.feedLike(feedId, memberId);
+        List<FeedResponseDTO> feedResponseDTOS = feedService.feedListOrderByLikeCount(1);
+
+        // then
+        assertThat(feedResponseDTOS.get(0).getContent()).isEqualTo("9");
+        assertThat(feedResponseDTOS.size()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("인증글 리스트 - 사용자기준 최신 순")
+    void feedlistbymember(){
+
+        // given
+        MemberRequestDto requestDto = MemberRequestDto.builder().nickname("test").build();
+        LevelRequestDto levelRequestDto = LevelRequestDto.builder().name("testLev").exp(5).build();
+        Level level = levelService.createLevel(levelRequestDto);
+        Member member = memberService.createMember(requestDto);
+
+        levelRepository.save(level);
+        memberRepository.save(member);
+
+        Feed feed1 = Feed.builder().content("1").member(member).build();
+        Feed feed2 = Feed.builder().content("2").member(member).build();
+        Feed feed3 = Feed.builder().content("3").member(member).build();
+        Feed feed4 = Feed.builder().content("4").member(member).build();
+        Feed feed5 = Feed.builder().content("5").member(member).build();
+        Feed feed6 = Feed.builder().content("6").member(member).build();
+        Feed feed7 = Feed.builder().content("7").member(member).build();
+        Feed feed8 = Feed.builder().content("8").member(member).build();
+        Feed feed9 = Feed.builder().content("9").member(member).build();
+        Feed feed10 = Feed.builder().content("10").member(member).build();
+
+        Feed feed11 = Feed.builder().content("11").build();
+        Feed feed12 = Feed.builder().content("12").build();
+        Feed feed13 = Feed.builder().content("13").build();
+        Feed feed14 = Feed.builder().content("14").build();
+        Feed feed15 = Feed.builder().content("15").build();
+        Feed feed16 = Feed.builder().content("16").build();
+        Feed feed17 = Feed.builder().content("17").build();
+        Feed feed18 = Feed.builder().content("18").build();
+        Feed feed19 = Feed.builder().content("19").build();
+
+        feedRepository.saveAll(List.of(feed1,feed2,feed3,feed4,feed5,feed6,feed7,feed8,feed9,feed11,feed12,feed13,feed14,feed15,feed16,feed17,feed18,feed19));
+        feedRepository.save(feed10);
+
+        // when
+        Member findMember = memberRepository.findMemberByNickname("test").orElseThrow(() -> new RuntimeException("찾을 수 없음"));
+        Long memberId = findMember.getMemberId();
+        List<FeedResponseDTO> feedResponseDTOS = feedService.feedListByMember(memberId, 1);
+
+        // then
+        assertThat(feedResponseDTOS.stream().map(FeedResponseDTO::getContent).collect(Collectors.toList())).contains("10","2","3","4","5","6","7","8","9");
+        assertThat(feedResponseDTOS.size()).isEqualTo(9);
+    }
+
 }
