9bfac1a5150ba013750f442f01d256009c87b252
KarmaPol <kkhkr98@gmail.com>
2023-09-15 16:38:57 +0900

    feat : add test code for feedList

--- src/main/java/dopamine/backend/feed/repository/FeedRepository.java
+++ src/main/java/dopamine/backend/feed/repository/FeedRepository.java
@@ -3,5 +3,8 @@ package dopamine.backend.feed.repository;
 import dopamine.backend.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    Optional<Feed> findFeedByContent(String content);
 }
