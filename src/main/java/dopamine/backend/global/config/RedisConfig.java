ed5bab771b9da1d1b04e19ab258ded2456df3c19
gabang2 <offbeat1020@naver.com>
2023-09-16 15:54:24 +0900

    refactor : 패키지 구조 변경

--- src/main/java/dopamine/backend/global/config/RedisConfig.java
+++ src/main/java/dopamine/backend/global/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package dopamine.backend.config;
+package dopamine.backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
