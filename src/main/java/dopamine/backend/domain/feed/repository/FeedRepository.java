545f7dcf820ca6ab4eaf65df213f8d6547510de0
gabang2 <offbeat1020@naver.com>
2023-09-16 16:48:14 +0900

    chore : successCnt추가

--- src/main/java/dopamine/backend/domain/feed/repository/FeedRepository.java
+++ src/main/java/dopamine/backend/domain/feed/repository/FeedRepository.java
@@ -1,10 +1,13 @@
 package dopamine.backend.domain.feed.repository;
 
 import dopamine.backend.domain.feed.entity.Feed;
+import dopamine.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     Optional<Feed> findFeedByContent(String content);
+    List<Feed> findFeedByMember(Member member);
 }
