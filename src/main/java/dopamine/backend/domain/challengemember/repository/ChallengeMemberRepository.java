cc7f80bb6367f2a59917c746de9d218f9137b465
KarmaPol <kkhkr98@gmail.com>
2023-09-19 19:50:19 +0900

    fix : fix annotation in Put, Post Controllers

--- src/main/java/dopamine/backend/domain/challengemember/repository/ChallengeMemberRepository.java
+++ src/main/java/dopamine/backend/domain/challengemember/repository/ChallengeMemberRepository.java
@@ -1,9 +1,15 @@
 package dopamine.backend.domain.challengemember.repository;
 
+import dopamine.backend.domain.challenge.entity.Challenge;
 import dopamine.backend.domain.challengemember.entity.ChallengeMember;
+import dopamine.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChallengeMemberRepository extends JpaRepository<ChallengeMember, Long> {
+
+    Optional<ChallengeMember> findChallengeMemberByChallengeAndMember(Challenge challenge, Member member);
 }
