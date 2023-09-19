b059b6011ba1e67b8c931311d9c753e387622b0c
gabang2 <offbeat1020@naver.com>
2023-09-19 14:45:53 +0900

    feat : 사용자 기준 월 별로 조회

--- src/main/java/dopamine/backend/domain/feed/repository/FeedRepository.java
+++ src/main/java/dopamine/backend/domain/feed/repository/FeedRepository.java
@@ -4,10 +4,13 @@ import dopamine.backend.domain.feed.entity.Feed;
 import dopamine.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     Optional<Feed> findFeedByContent(String content);
     List<Feed> findFeedByMember(Member member);
+
+    public List<Feed> findFeedByMemberAndCreatedDateBetweenOrderByCreatedDate(Member member, LocalDateTime startDate, LocalDateTime finishDate);
 }
