28f11833d60b4fc2ebdfab317bdee67a55630730
gabang2 <offbeat1020@naver.com>
2023-09-20 00:45:05 +0900

    feat #76 : feed list backoffice 구현

--- src/main/java/dopamine/backend/backoffice/controller/FeedBackofficeController.java
+++ src/main/java/dopamine/backend/backoffice/controller/FeedBackofficeController.java
@@ -1,5 +1,8 @@
 package dopamine.backend.backoffice.controller;
 
+import dopamine.backend.domain.feed.entity.Feed;
+import dopamine.backend.domain.feed.repository.FeedRepository;
+import dopamine.backend.domain.level.entity.Level;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,14 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.List;
+
 @Controller
 @EnableWebMvc
 @RequestMapping("/backoffice/feed")
 @RequiredArgsConstructor
 public class FeedBackofficeController {
+    private final FeedRepository feedRepository;
     @GetMapping
     public String feed(Model model) {
-        model.addAttribute("data", "안녕");
+        List<Feed> feeds = feedRepository.findAll();
+        model.addAttribute("feeds", feeds);
         return "feed/feed";
     }
 }
