42ff119e1720448160b822291f34e31dd7c949d8
gabang2 <offbeat1020@naver.com>
2023-09-19 22:32:16 +0900

    feat #76 : 레벨 조회 & 삭제 기능 구현

--- src/main/java/dopamine/backend/backoffice/controller/BackofficeController.java
+++ src/main/java/dopamine/backend/backoffice/controller/BackofficeController.java
@@ -1,38 +1,63 @@
 package dopamine.backend.backoffice.controller;
 
+import dopamine.backend.domain.level.entity.Level;
+import dopamine.backend.domain.level.repository.LevelRepository;
+import dopamine.backend.domain.level.service.LevelService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 @Controller
 @EnableWebMvc
 @RequestMapping("/backoffice")
+@RequiredArgsConstructor
 public class BackofficeController {
 
+    private final LevelRepository levelRepository;
+    private final LevelService levelService;
+
     @GetMapping
-    public String home(){
+    public String home() {
         return "home/home";
     }
 
-    @GetMapping("/level")
-    public String level(){
-        return "level";
-    }
-
-    @GetMapping("/member")
-    public String member(){
-        return "member";
+    @GetMapping("/challenge")
+    public String challenge(Model model) {
+        model.addAttribute("data", "안녕");
+        return "challenge/challenge";
     }
 
     @GetMapping("/feed")
-    public String feed(){
-        return "feed";
+    public String feed(Model model) {
+        model.addAttribute("data", "안녕");
+        return "feed/feed";
     }
 
-    @GetMapping("/challenge")
-    public String challenge(){
-        return "challenge";
+    @GetMapping("/level")
+    public String level(Model model) {
+        List<Level> levels = levelRepository.findAll();
+        model.addAttribute("levels", levels);
+        return "level/level";
     }
+
+    @GetMapping("/level/{levelId}/delete")
+    public String levelDelete(@PathVariable("levelId") Long levelId) {
+        levelService.deleteLevel(levelId);
+        return "redirect:/backoffice/level";
+    }
+
+    @GetMapping("/level/create")
+    public String levelCreate(Model model) {
+        List<Level> levels = levelRepository.findAll();
+        model.addAttribute("levels", levels);
+        return "level/levelCreate";
+    }
+
 
 }
